4c98dd3d96cc34995676d451081b2048f8962b88
Franco Román Meola <franco.meola@icloud.com>
2014-09-03 16:22:13 -0300

    Agregada la función de Determinantes de Diego.

--- MVMGRC/src/grcar/Grcar.java
+++ MVMGRC/src/grcar/Grcar.java
@@ -35,7 +35,25 @@ public class Grcar {
 		}
 	}
 	
+	public static int determinant(int[][] mat) {
+		int result = 0;
+		if (mat.length == 2) {
+			result = mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
+			return result;
+		}
+		for (int i = 0; i < mat[0].length; i++) {
+			int temp[][] = new int[mat.length - 1][mat[0].length - 1];
+			for (int j = 1; j < mat.length; j++) {
+				System.arraycopy(mat[j], 0, temp[j - 1], 0, i);
+				System.arraycopy(mat[j], i + 1, temp[j - 1], i, mat[0].length
+						- i - 1);
+			}
+			result += mat[0][i] * Math.pow(-1, i) * determinant(temp);
+		}
+		return result;
+	}
+	
 	public static void main(String[] args) {
-		printMatrix(getGrcarMatrix(10));
+		System.out.println(determinant(getGrcarMatrix(10)));
 	}
 }
